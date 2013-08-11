83883158c6806b61e94248388ad2704de7b4bfab
Steffen Beyer <steffen@beyer.io>
2013-08-11 04:29:08 +0200

    check multiple sensors, write CSV, logout

--- weatherlog.js
+++ weatherlog.js
@@ -1,13 +1,59 @@
 var CCU_HOSTNAME = 'homematic.fritz.box',
     USERNAME     = 'pi',
-    PASSWORD     = 'nopassword';
+    PASSWORD     = 'nopassword',
+    DEBUG        = true,
+    
+    SENSORS      = [
+      { address: 'KEQ0048734', name: 'innen' },
+      { address: 'KEQ0053713', name: 'aussen' },
+      { address: 'KEQ0053703', name: 'Dachboden' }
+    ],
+    
+    CSV_FILENAME = '/home/pi/weather.csv';
 
-var http         = require('http'),
+// main
+
+var async   = require('async'),
+    fs      = require('fs'),
+    http    = require('http'),
+    moment  = require('moment'),
+    csvStream,
     session;
 
-getSensorData('KEQ0053713', function gotResult(data) {
-  console.log(data);
+var writeCsvHeader = ! fs.existsSync(CSV_FILENAME),
+    csvStream      = fs.createWriteStream(CSV_FILENAME, { flags: 'a' }),
+    sensorCount    = SENSORS.length;
+
+if (writeCsvHeader) {
+  var fields = 'Zeit';
+  for (var i = 0; i < sensorCount; i++) {
+    var sensor = SENSORS[i];
+    fields += ',Temperatur ' + sensor.name + ',Luftfeuchte ' + sensor.name;
+  }
+  csvStream.write(fields + "\r\n");
+}
+
+var funs    = [],
+    fields  = [ moment().format() ],
+    running = sensorCount;
+for (var i = 0; i < sensorCount; i++) {
+  funs[i] = function(f) {
+    return function(callback) {
+      getSensorData(SENSORS[f].address, function gotResult(data) {
+        fields[f * 2 + 1] = parseFloat(data.TEMPERATURE);
+        fields[f * 2 + 2] = parseInt(data.HUMIDITY);
+        callback();
+      });
+    };
+  }(i);
+}
+
+async.series(funs, function allDone() { // allow only one request at a time
+  csvStream.write(fields.join(',') + "\r\n");
+  ccuLogout(); // function() {});
 });
+
+// CCU API
 
 function getSensorData(address, callback) {
   ccuMethod('Interface.getParamset', {
@@ -16,6 +62,8 @@ function getSensorData(address, callback) {
     paramsetKey: 'VALUES'
   }, callback);
 }
+
+// CCU RPC
 
 function ccuMethod(method, params, callback) {
   if (! session) {
@@ -63,9 +111,19 @@ function ccuRpc(method, params, callback) {
       if (result.error) {
         throw 'CCU RPC error ' + result.error.code + ': ' + result.error.message;
       } else {
+        if (DEBUG) {
+          console.log('<= ' + body);
+        }
         callback(JSON.parse(body));
       }
     });
   });
+  if (DEBUG) {
+    console.log('=> ' + query);
+  }
   req.end(query);
+}
+
+function ccuLogout(callback) {
+  ccuMethod('Session.logout', {}, callback !== undefined ? callback : function() {});
 }
